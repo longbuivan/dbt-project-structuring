f3b4c73f1d496477ddc97ce7826dfbecf3c82dd3
longbuivan <longbuivan95@gmail.com>
2023-01-11 11:24:36 +0700

    new dbt

---
+++ models/staging/google-sheets/campaign_tracking.sql
@@ -0,0 +1,15 @@
+-- public.sheet1 definition
+
+-- Drop table
+
+-- DROP TABLE public.sheet1;
+
+CREATE TABLE public.sheet1 (
+	"Name" text NULL,
+	"Class" text NULL,
+	subject text NULL,
+	"_airbyte_ab_id" varchar NULL,
+	"_airbyte_emitted_at" timestamptz NULL,
+	"_airbyte_normalized_at" timestamptz NULL,
+	"_airbyte_sheet1_hashid" text NULL
+);
